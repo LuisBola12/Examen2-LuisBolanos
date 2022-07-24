0b8248ef23608ca92ce16509c31e284ba64d1eaa
LuisBola12 <luibv1202@gmail.com>
2022-07-24 16:05:32 -0600

    Implementacion de carpeta data en la cual se manejan la informacion de la cantidad de bebidas y monedas que hay en el storage

---
+++ maquina-de-bebidas/src/Data/drinks.js
@@ -0,0 +1,14 @@
+export const drinks = [
+    {name:"CocaCola",
+    price:500,
+    quantity:10},
+    {name:"Pepsi",
+    price:600,
+    quantity:8},
+    {name:"Fanta",
+    price:550,
+    quantity:10},
+    {name:"Sprite",
+    price:725,
+    quantity:15},
+]
